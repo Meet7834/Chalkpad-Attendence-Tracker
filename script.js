ded61c698a88106710161f858dae0b80a6240992
Meet <proobmeet7834@gmail.com>
2022-12-10 00:04:04 +0530

    Working for the first time

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const table = document.querySelector("#attendanceResultDiv > table > tbody")
-const noOfSubjects = table.children.length;
-
 const addCell = (row, extraClass, shortAtd) => {
     let cell = document.createElement("td");
     cell.setAttribute("width", "2%");
@@ -27,12 +24,12 @@ const addHeadingCell = (headingRow) => {
 const addInstructions = () => {
     let div = document.querySelector("#attendanceResultDiv");
     let green = document.createElement("p");
-    green.innerText = 'Green: How many classes you can bunk without your attendence falling below 75%.';
+    green.innerText = 'Green: How many classes you can bunk without it falling below 75%.';
     green.setAttribute("style", "color: #82CD47");
     let red = document.createElement("p");
     red.innerText = 'Red: How many classes you have to attend to make your attendence 75%';
     red.setAttribute("style", "color: #DC3535 ");
-    
+
     div.append(green);
     div.append(red);
 }
@@ -68,19 +65,28 @@ const noOfextraClass = (attended, deliverd) => {
 }
 
 const mainFunc = () => {
-    const headingRow = document.querySelector('#attendanceResultDiv > table > tbody > tr:nth-child(1)');
-    addHeadingCell(headingRow);
-    for (let i = 2; i <= noOfSubjects; i++) {
-        const row = document.querySelector(`#attendanceResultDiv > table > tbody > tr:nth-child(${i})`);
-        // const subject = row.children[2];
-        const attended = row.children[6];
-        const deliverd = row.children[9];
-        const numAtn = parseInt(attended.innerText);
-        const numDel = parseInt(deliverd.innerText);
-        const {extraClass, per, shortAtd} = noOfextraClass(numAtn, numDel);
-        addCell(row, extraClass, shortAtd);
+    try {
+        const table = document.querySelector("#attendanceResultDiv > table > tbody")
+        const noOfSubjects = table.children.length;
+        const headingRow = document.querySelector('#attendanceResultDiv > table > tbody > tr:nth-child(1)');
+        addHeadingCell(headingRow);
+        for (let i = 2; i <= noOfSubjects; i++) {
+            const row = document.querySelector(`#attendanceResultDiv > table > tbody > tr:nth-child(${i})`);
+            const attended = row.children[6];
+            const deliverd = row.children[9];
+            const numAtn = parseInt(attended.innerText);
+            const numDel = parseInt(deliverd.innerText);
+            const {extraClass, per, shortAtd} = noOfextraClass(numAtn, numDel);
+            addCell(row, extraClass, shortAtd);
+        }
+        addInstructions();
+    }catch{
+        setTimeout(function(){
+            mainFunc();
+        }, 2000)
     }
-    addInstructions();
 };
 
-mainFunc();
+setTimeout(function () {
+    mainFunc();
+}, 2000);
